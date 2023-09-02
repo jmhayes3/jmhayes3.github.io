3509a7a13a8eaf42953efd8bd438deb841b82b0d
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2023-09-02 15:52:03 -0500

    update title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Next.js on GitHub Pages',
-  description: 'A Next.js web application on GitHub Pages',
+  title: 'jmhayes3.github.io',
+  description: 'A blog or something?',
 }
 
 export default function RootLayout({
