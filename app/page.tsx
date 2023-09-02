73377a89a403a88759c54ab0ff8582300847be96
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2023-09-02 14:58:01 -0500

    fix bad syntax

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <h1>
-        Hello.
-    </h1>
-    <div>
-      <Link href="/about">About</Link>
-    </div>
+    <>
+      <h1>Home</h1>
+      <div>
+        <Link href="/about">About</Link>
+      </div>
+    </>
   )
 }
